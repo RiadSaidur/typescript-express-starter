58a139b6ccc81fc892715cd3194e5fc21bce2cd2
RiadSaidu <reidvai1998@gmail.com>
2022-01-23 16:27:26 +0600

    starter project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import Express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
 
 const app = Express()
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+// Middleware
+app.use(Express.json())
+
+// Routes
+
+
+// MongoDB connection
+const USERNAME = process.env.USER_NAME
+const PASSWORD = process.env.PASSWORD
+const DATABASE = process.env.DB_NAME
+const mongodbURI = `mongodb+srv://${USERNAME}:${PASSWORD}@netjobs.jglqn.mongodb.net/${DATABASE}?retryWrites=true&w=majority`
+
+mongoose.connect(mongodbURI)
+
+mongoose.connection.on('connected', () => {
+  console.log('connceted to DB')
+  app.listen(PORT, () => console.log(`server stated on ${PORT}`))
+})
